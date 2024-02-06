d8e149e25728b7b92a1efffa16add3bc34e53008
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-02-06 16:11:37 -0300

    Create aula4.sql

---
+++ aula4.sql
@@ -0,0 +1,42 @@
+-- Encontre a quantidade de eventos que cada pessoa participou, os que mais participaram aparecem por primeiro e quem nunca participou deve aparecer 0.
+SELECT P.Nome, COUNT(EP.IDEventoPessoa) AS "Qtde de eventos" FROM Pessoa P
+LEFT JOIN EventoPessoa EP
+ON P.IDPessoa = EP.IDPessoa
+GROUP BY P.Nome
+ORDER BY COUNT(EP.IDEventoPessoa) DESC
+
+
+
+--5. Preciso controlar se as reuniões estão sendo realizadas nas salas com a capacidade correta.
+--Liste as Salas, Eventos, Capacidade das salas e Quantidade de Participantes.
+--Crie uma coluna chamada "Avaliação da Capacidade" com dados como: "Ultrapassou o limite", "Limite ok".
+--Dica: Use "Case When"
+
+SELECT S.Nome, E.Descricao, S.Capacidade, COUNT(EP.PapelEvento) AS 'Participantes',
+	CASE
+		WHEN S.Capacidade >= COUNT(EP.IDPessoa) THEN 'Limite OK'
+		WHEN S.Capacidade < COUNT(EP.IDPessoa) THEN 'Ultrapassou o limite'
+	END AS 'Avaliação da capacidade'
+FROM Evento E
+INNER JOIN Sala S
+ON S.IDSala = E.IDSala
+INNER JOIN EventoPessoa EP
+ON E.IDEvento = EP.IDEvento
+GROUP BY S.Nome, E.Descricao, S.Capacidade
+
+--Encontre com sub-select, os eventos que iriam ter um limite ultrapassado, mas que não tiverasm 
+--por causa de algumas pessoas que faltaram no dia,
+--criei uma VIEW para esse select
+
+SELECT
+	S.Nome, 
+	E.Descricao,
+	S.Capacidade,
+	COUNT(EP.PapelEvento) AS 'Participantes'
+FROM Evento E
+INNER JOIN Sala S
+ON S.IDSala = E.IDSala
+INNER JOIN EventoPessoa EP
+ON E.IDEvento = EP.IDEvento
+GROUP BY S.Nome, E.Descricao, S.Capacidade
+HAVING S.Capacidade < COUNT(EP.IDPessoa) AND S.Capacidade >= SUM(CASE WHEN EP.Presenca = 1 THEN 1 ELSE 0 END)
