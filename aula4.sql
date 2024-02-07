d5cc1a7ae44c04817df8baf825d7fc2586a45c89
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-02-07 13:59:22 -0300

    Update aula4.sql

--- aula4.sql
+++ aula4.sql
@@ -44,6 +44,37 @@ ON E.IDEvento = EP.IDEvento
 GROUP BY S.Nome, E.Descricao, S.Capacidade, E.IDEvento
 HAVING S.Capacidade < COUNT(EP.IDPessoa) AND S.Capacidade >= SUM(CASE WHEN EP.Presenca = 1 THEN 1 ELSE 0 END)
 
+-- OUTRA RESOLUÇÃO USANDO IN
+SELECT -- selecionou os eventos que haviam excedido a capacidade somente com os convidados, porém os presentes não excederam aquele valor
+	S.Nome as Sala,
+	E.Descricao as Evento,
+	S.Capacidade as CapacidadeSala,
+	COUNT(EP.IDEventoPessoa) as Presentes
+
+FROM Evento as E
+
+INNER JOIN Sala as S
+ON S.IDSala = E.IDSala
+
+INNER JOIN EventoPessoa as EP
+ON  EP.IDEvento = E.IDEvento
+
+GROUP BY EP.Presenca, E.IDEvento, S.Nome, S.Capacidade, E.Descricao
+HAVING EP.Presenca = 1 AND S.Capacidade >= COUNT(EP.IDEventoPessoa) AND E.IDEvento IN (
+
+	SELECT 
+		E.IDEvento
+	FROM Evento as E
+
+	INNER JOIN Sala as S
+	ON S.IDSala = E.IDSala
+
+	INNER JOIN EventoPessoa as EP
+	ON  EP.IDEvento = E.IDEvento
+
+	GROUP BY E.IDEvento, S.Capacidade
+	HAVING COUNT(EP.IDEventoPessoa) > S.Capacidade)
+
 --SUB-SELECT e IN:
 SELECT * FROM Evento E
 INNER JOIN EventoEquipamento EQ
