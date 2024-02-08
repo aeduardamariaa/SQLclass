b9cdef090f9a73941194468610766ad749e9bd68
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2024-02-08 12:58:32 -0300

    Update aula5.sql

--- aula5.sql
+++ aula5.sql
@@ -1,22 +1,25 @@
-CREATE FUNCTION seiLa (@tipo VARCHAR(10))
+CREATE FUNCTION fnProcurarEventoAntigo (@tipo VARCHAR(10))
 RETURNS TABLE AS
-RETURN(
-	
+RETURN
+(
+	SELECT TOP 1
+		E.Descricao AS 'Nome evento',
+		S.Nome AS 'Nome sala',
+		COUNT(CASE WHEN EP.Presenca = 1 THEN 1 END) AS 'Presentes',
+		CONVERT(VARCHAR(10), E.DtHrInicio, 103) AS 'Data Inicio',
+		CONVERT(VARCHAR(5), E.DtHrInicio, 108) AS 'Data Fim'
+	FROM Evento E
+	INNER JOIN Sala S
+	ON E.IDSala = S.IDSala
+	INNER JOIN EventoPessoa EP
+	ON EP.IDEvento = E.IDEvento
+	WHERE E.Descricao LIKE CONCAT('%', @tipo, '%')
+	GROUP BY
+		E.IDEvento,
+		E.Descricao,
+		S.Nome,
+		E.DtHrInicio
+	ORDER BY E.DtHrInicio
 )
 
-SELECT * FROM Evento
-SELECT * FROM Sala
-SELECT * FROM EventoPessoa
-
-SELECT 
-	E.Descricao AS 'Nome evento',
-	S.Nome AS 'Nome sala',
-	SUM(CASE WHEN EP.Presenca = 1 THEN 1 ELSE 0 END) AS 'Presentes',
-	CONVERT(VARCHAR(10), E.DtHrInicio, 103) AS 'Data Inicio',
-	CONVERT(VARCHAR(10), E.DtHrFim, 103) AS 'Data Fim'
-FROM Evento E
-INNER JOIN Sala S
-ON E.IDSala = S.IDSala
-INNER JOIN EventoPessoa EP
-ON EP.IDEvento = E.IDEvento
-GROUP BY S.Nome, E.Descricao, S.Capacidade, E.IDEvento, E.DtHrInicio, E.DtHrFim
+SELECT * FROM dbo.fnProcurarEventoAntigo('Reuniao')
